7cfed3599794a8c86b59ed21cb3e17e01f1e9a2a
Christian Richter <richterchristian25@gmail.com>
2025-11-18 18:10:51 +0100

    S4 A2

--- Serie4/A1/exact_search_fm_idx.py
+++ Serie4/A1/exact_search_fm_idx.py
@@ -43,12 +43,12 @@ def getBWT(suffixArray, text):
 def getFirstAndRanks(bwt):
 
     # calc ranks
-    counts = np.zeros(lenAlphabet, dtype=int)
-    ranks = np.zeros((lenAlphabet, len(bwt)), dtype=int)
+    counts = defaultdict(int)
+    ranks = defaultdict(defaultdict(int))
     for i, char in enumerate(bwt):
         if i > 0:    
-            ranks[:,i] = ranks[:,i-1].copy()
-        ranks[ord(char),i] += 1
+            ranks[i] = ranks[i-1].copy()
+        ranks[i][ord(char)] += 1
         counts[ord(char)] += 1
 
     # calc first
@@ -70,32 +70,38 @@ def getFirstAndRanks(bwt):
 def fm_idx_search(pattern, first, ranks, len_text):
     left = 0
     right = len(len_text) - 1
-    for i in range(len(pattern)-1, -1, -1):
+    itertext = f'pattern search\n###############\nstart with empty pattern\nL: {left}\t R: {right}\n'
+    for i in range(len(pattern)-1, -1, -1):        
         char = pattern[i]
+        itertext += f'Iteration: {len(pattern)-1-i}\n---------------------\ncurrent pattern suffix: {pattern[i:]}\n'
         try:
             left = first[char] + (ranks[ord(char), left-1] if left > 0 else 0)
             right = first[char] + ranks[ord(char), right] - 1
             #print(first[char], (ranks[ord(char), left-1] if left > 0 else 0), ranks[ord(char), right])
             #print(f'After processing char "{char}": left={left}, right={right}')
+            itertext += f'L+: {left}\t R+: {right}\n'
         except KeyError:
-            return -1
+            return -1, itertext
         if left > right:
-            return -1
-        
-    return left, right
+            return -1, itertext
+    itertext += '\n'    
+    return (left, right), itertext
 
 
 
 def main():
     pattern = sys.argv[1]
-    textFpath = 'text2.txt'
-    with open(textFpath, 'r') as f:
-        lines = f.readlines()
-        #text = ''.join(lines)
-        text = ''
-        for line in lines:
-            text += line.replace('\n', '')
-        text = text.replace(' ', '_')
+    if len(sys.argv) == 3:
+        text = sys.argv[2]
+    else:
+        textFpath = 'text2.txt'
+        with open(textFpath, 'r') as f:
+            lines = f.readlines()
+            #text = ''.join(lines)
+            text = ''
+            for line in lines:
+                text += line.replace('\n', '')
+            text = text.replace(' ', '_')
     text+='$'
 
     suffixes = getSuffixArray(text)
@@ -109,26 +115,31 @@ def main():
         '| Suffix': suffixes[:,0],
         '| Suffix Array': suffixArray,
         '| BWT': bwt,
-        '| sorted BWT': np.sort(bwt),
+        '| sorted BWT (F)': np.sort(bwt),
         '| ranks: '+str([str(char) for char in bwt]): [ranks_vis[i,:] for i in range(ranks_vis.shape[0])]
     })
-    with open('control_table.txt', 'w') as f:
-        f.write(df.to_string(index=False))
-        f.write('first: ' + str(first) + '\n')
     #print(df.to_string(index=False))
-    #print('first: ', first)
-    
-    result = fm_idx_search(pattern, first, ranks, text)
-    positions = suffixArray[result[0]:result[1]+1]
+    #print('first: ', first)   
+    result, itertext = fm_idx_search(pattern, first, ranks, text)    
+    result_text = ''
+    org_text = text    
     if result == -1:
-        print(f'Pattern "{pattern}" not found in text.')
+        result_text = f'Pattern "{pattern}" not found in text: '
+        print(result_text)
+        print(org_text)
     else:
-        print(f'Pattern found at positions {positions} in text.')
+        positions = suffixArray[result[0]:result[1]+1]
+        result_text = f'Pattern found at positions {np.sort(positions).tolist()} in text: '
+        print(result_text)            
+        for pos in sorted(positions, reverse=True):
+                text = text[:pos] + color.RED + text[pos:pos+len(pattern)] + color.END + text[pos+len(pattern):]
+        print(text.replace('_', ' '))    
 
-    for pos in sorted(positions, reverse=True):
-            text = text[:pos] + color.RED + text[pos:pos+len(pattern)] + color.END + text[pos+len(pattern):]
-
-    print(text.replace('_', ' '))
+    with open('output.txt', 'w') as f:
+        f.write(df.to_string(index=False))
+        f.write('\nfirst: ' + str(first) + '\n')
+        f.write(itertext)
+        f.write(result_text + f'{org_text}')
 
 
 if __name__ == "__main__":
---
+++ Serie4/A2/trimmomatic_cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+input=../../Data/SRR1258470/SRR1258470.fastq.gz
+output=SRR1258470_trimmed.fastq.gz
+trimmomatic SE -phred33 -trimlog SRR1248470_trimlog.log $input $output HEADCROP:10 TAILCROP:10 LEADING:28 TRAILING:28 MINLEN:20
