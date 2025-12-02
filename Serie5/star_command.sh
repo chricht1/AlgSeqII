3ded92a0748b476d7a175103a8accc5d29ff145f
amrms <christian.richter@student.uni-halle.de>
2025-12-02 01:16:32 +0100

    Serie6

---
+++ Serie5/star_command.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# create index
+STAR --runThreadN 4 \
+--runMode genomeGenerate \
+--genomeDir Data/Saccharomyces_Genome_R64/Star/Star_Genome_Idx \
+--genomeFastaFiles Data/Saccharomyces_Genome_R64/NCBI_RefSeq_Assembly/GCF_000146045.2_R64_genomic.fna \
+--sjdbGTFfile Data/Saccharomyces_Genome_R64/NCBI_RefSeq_Assembly/genomic.gtf \
+--sjdbOverhang 80
+
+# mapping:
+STAR --runThreadN 4 \
+--genomeDir Data/Saccharomyces_Genome_R64/Star/Star_Genome_Idx \
+--readFilesIn Data/SRR1258470/SRR1258470_trimmed.fastq \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix Data/Saccharomyces_Genome_R64/Star/star_map_sorted_
+
+
+
+#Fragen:
+#- was sind die vielen blauen streifen? Verbindungen für Reads?
+#	- sind das splice junctions?
+#- was ist junctions track in IGV über bam file?
+#- was sind das unter sequence für buchstaben?
