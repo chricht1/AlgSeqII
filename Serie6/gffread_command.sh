fc847d3e8aec1d2172f66373f2b180d618cb5d1f
amrms <christian.richter@student.uni-halle.de>
2025-12-02 10:37:14 +0100

    s6 refine

---
+++ Serie6/gffread_command.sh
@@ -0,0 +1,3 @@
+gffread -w Data/Saccharomyces_RefGenome_R64/NCBI_RefSeq_Assembly/transcripts_gffread.fa \
+-g Data/Saccharomyces_RefGenome_R64/NCBI_RefSeq_Assembly/GCF_000146045.2_R64_genomic.fna \
+Data/Saccharomyces_RefGenome_R64/NCBI_RefSeq_Assembly/genomic.gff
