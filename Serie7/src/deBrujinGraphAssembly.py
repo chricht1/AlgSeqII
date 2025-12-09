ea5240233b97c811bb81331c4c502737c324ec0b
amrms <christian.richter@student.uni-halle.de>
2025-12-09 10:39:32 +0100

    s7 progress

--- Serie7/src/deBrujinGraphAssembly.py
+++ Serie7/src/deBrujinGraphAssembly.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import numpy as np
 
 class DeBrujinGraph:
     def __init__(self, sequences, k):
@@ -28,26 +29,67 @@ class DeBrujinGraph:
     
         
 class Assembly:
-    def __init__(self, deBrujinGraph:DeBrujinGraph):
+    def __init__(self, deBrujinGraph:DeBrujinGraph, outFile:str):
         self.graph = deBrujinGraph
+        self.outFile = outFile
 
     def findAssemblyEuler(self, nodeDegrees, outFile): # Algorihtm of Hierholzer
-        g = self.graph
-        edgeMarks = defaultdict(lambda: defaultdict(int))
+        self.numAssemblies = 0
+        self.edgeMarks = defaultdict(lambda: defaultdict(int))
         
-
-        for node in nodeDegrees:
-            if node[0] == 0 and node[1] == 1:
-                startNode = node
+        while 1:
+            assembly = self.findEulerPath(nodeDegrees)
+            if assembly is None:
+                break
+            
         node = startNode
         assembly = startNode.key()
 
+        
+
+    def findEulerPath(self, nodeDegrees):
+        g = self.graph
+        node = self.findStartNode(nodeDegrees)
+        
         while(node is not None):
-            for succBase, edgeCount in g[node]: 
-                if edgeMarks[node][succBase] < edgeCount:
+            for succBase, edgeCount in g[node].items():
+                if self.edgeMarks[node][succBase] < edgeCount:
                     nextNode = g[node[1:]+succBase]
-                    edgeMarks[node][succBase] += 1
+                    self.edgeMarks[node][succBase] += 1
                     assembly += succBase
                     node = nextNode 
                     break
-                
+
+        return assembly
+
+
+    def findStartNode(self, nodeDegrees):
+        startNode = None
+        for node in nodeDegrees:
+            if node == [0,1]:
+                startNode = node
+        '''if startNode is None:
+            for node in nodeDegrees:
+                if node[0] % 2 == 1 and node[1] % 2 == 1 and node[0] < node[1] and self.hasUmarkedEdges():'''
+        #I calculated an De-Brujin Graph from read sequences. Now I should find an assembly and thus an Euler Path or multiple Euler Paths. The first criteria to start  
+        return startNode
+    
+
+    def hasUmarkedEdges(self):
+        g = self.graph
+        for node in g:
+            for succBase in g[node]:
+                if self.edgeMarks[node][succBase] < g[node][succBase]:
+                    return True
+        return False
+    
+
+    def writeAssemblyToFile(self, assembly, assemblyName):
+        lineLen = 81
+        if self.numAssemblies == 0:
+
+        with open(self.outFile, 'a') as f:
+            f.write(f'>{assemblyName}\n')
+            for i in range(len(assembly)//lineLen):
+                f.write(assembly[i*lineLen:(i+1)*lineLen] + '\n')
+            f.write(assembly[(len(assembly)//lineLen)*lineLen:] + '\n')
